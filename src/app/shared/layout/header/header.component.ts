3221ceda9fa4851e80326939beb3b59c01ab644d
ibdkn <ibudiakina@gmail.com>
2024-07-11 18:57:05 +0300

    added logic for login and logout

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {CategoryType} from "../../../../types/category.type";
+import {AuthService} from "../../../core/auth/auth.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,7 +12,40 @@ import {CategoryType} from "../../../../types/category.type";
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  @Input() categories!: CategoryType[]
-  constructor() {
+  isLogged: boolean = false;
+  @Input() categories!: CategoryType[];
+
+
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router) {
+    this.isLogged = this.authService.getIsLoggedIn();
+  }
+
+  ngOnInit(): void {
+    this.authService.isLogged$
+      .subscribe((isLoggedIn: boolean) => {
+        this.isLogged = isLoggedIn;
+      })
+  }
+
+  logout(): void {
+    this.authService.logout()
+      .subscribe({
+        next: () => {
+          this.doLogout();
+        },
+        error: () => {
+          this.doLogout();
+        }
+      })
+  }
+
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+
+    this._snackBar.open('Вы вышли из системы');
+    this.router.navigate(['/login']);
   }
 }
